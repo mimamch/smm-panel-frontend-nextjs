053f965033d92fd6bcd86013199452e000141e9f
mimamch <mimamch28@gmail.com>
2022-05-06 11:57:56 +0700

    fix google fonts

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,11 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+          href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&display=swap"
           rel="stylesheet"
         />
       </Head>
